849210a4ff9fa40d5c62b7e83d8f759db4c73125
dominionized <dominique.begin.0@gmail.com>
2016-02-11 15:32:22 -0500

    Add stuff

--- anonymoustre/main.py
+++ anonymoustre/main.py
@@ -1,14 +1,14 @@
 import shodan
 import api_key
 import requests
+import pprint
+from functools import reduce
 
 
 def main():
-
     api = shodan.Shodan(api_key.SHODAN)
     # Wrap the request in a try/ except block to catch errors
     try:
-
         # Search Shodan
         results = api.search('apache')
 
@@ -23,25 +23,28 @@ def main():
         print('Error: %s' % e)
 
 
-def query_google_api():
-
+def query_google_api(ip_list):
+    url = "https://sb-ssl.google.com/safebrowsing/api/lookup"
     url_params = {
         "client": "anonymoustre",
         "key": api_key.GOOGLE_SAFE_BROWSING,
         "appver": "0.0.1",
         "pver": "3.1"
     }
+    req_body = str(len(ip_list)) + "\n" + reduce(lambda x, y: x+"\n"+y, ip_list)
 
-    req_body = "2\nhttp://sourceforge.net/\nhttp://google.com/"
+    print(req_body)
 
-    r = requests.post("https://sb-ssl.google.com/safebrowsing/api/lookup",
-                                params=url_params,
-                                data=req_body)
-    print(r.url)
-    print(r.headers)
+    r = requests.post(url, params=url_params, data=req_body)
+
     print(r.status_code)
     print(r.text)
 
+    if r.status_code == 204:
+        return # TODO change this
+
+def assoc_score(ip_list):
+    return map(lambda ip: (ip, 100), ip_list)
 
 if __name__ == "__main__":
-    query_google_api()
+    pprint(assoc_score(["192.168.1.1", "192.168.1.2", "192.168.0.43"]))
